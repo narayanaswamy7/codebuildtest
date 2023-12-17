3cbecae298599973c054347e3e344ae35ee4b765
Narayana Swamy <nswamy751@gmail.com>
2023-12-17 08:40:51 +0530

    first commit

---
+++ src/test/java/com/ns/codebuilddemo/CodebuilddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns.codebuilddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebuilddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
